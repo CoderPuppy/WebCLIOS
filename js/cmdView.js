f5e9edeed3ca63ddfad608c2adda2042b1d67ff1
Drew Young <drewyoung1@gmail.com>
2012-04-19 12:33:36 -0400

    Add new stream implementation

--- js/cmdView.js
+++ js/cmdView.js
@@ -1,4 +1,14 @@
-define(['require', 'exports', './stream'], function(require, exports, Stream) {
+define(['require', 'exports', './stream', './events'], function(require, exports, Stream, events) {
+	var EventEmitter = events.EventEmitter;
+	
+	function merge(dest, source) {
+		for(key in source) {
+			dest[key] = source[key];
+		}
+		
+		return dest;
+	}
+	
 	var CMDView = exports.CMDView = (function() {
 		function CMDView(runView, cmdContext, cmd, options) {
 			var self = this;
@@ -24,11 +34,55 @@ define(['require', 'exports', './stream'], function(require, exports, Stream) {
 			if(this.pipe == 'later') this.el.insertBefore(this.runView.el.children().last());
 			else this.el.appendTo(this.runView.el);
 			
+			this.stream = (function() {
+				var stream = {
+					write: function write() { // Write data out
+						this.emit.apply(this, ['__data__'].concat([].slice.call(arguments)));
+					},
+					data: function data(cb) { // On recieve data
+						this.on('data', cb);
+					},
+					contentType: function contentType(value) {
+						if(value) {
+							this._contentType = value;
+							
+							return this;
+						} else {
+							return this._contentType || 'application/json';
+						}
+					},
+					__write__: function() { // Write data in
+						this.emit.apply(this, ['data'].concat([].slice.call(arguments)));
+					},
+					__data__: function(cb) { // On write data
+						this.on('__data__', cb);
+					}
+				};
+				
+				EventEmitter.call(stream);
+				
+				stream = merge(stream, new EventEmitter());
+				
+				return stream;
+			})();
 			
-			this.stream = new Stream();
+			this.inData = [];
+			this.outData = [];
 			
-			this.stream.on('data', function(data) {
-				self.output.push(data);
+			this.input = new Stream();
+			this.output = new Stream();
+			
+			this.input.on('data', function() {
+				self.stream.__write__.apply(self.stream, arguments);
+				self.data = self.inData.concat(arguments);
+			});
+			
+			this.stream.__data__(function() {
+				self.output.write.apply(self.output, [].slice.call(arguments));
+				self.data = self.outData.concat(arguments);
+			});
+			
+			this.output.on('data', function(data) {
 				self.outputEl.append((self.outputEl.html().length > 0 ? '<br />' : '') + data);
 			});
 		}
